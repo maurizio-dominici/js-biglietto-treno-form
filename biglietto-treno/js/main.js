707d8877bdeabf271ee50641e1ea3d2fbf21b46b
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-10 17:20:18 +0200

    add function

--- biglietto-treno/js/main.js
+++ biglietto-treno/js/main.js
@@ -1,45 +1,33 @@
 // ^ RACCOLTA DATI
 const kmPrice = 0.21;
-const askName = prompt("come ti chiami?");
-const totalKm = Number(prompt("quanti km vuoi percorrere"));
-// const passengerAge = Number(prompt("quanti anni hai"));
-const passengerAge = prompt("sei maggiorenne, minorenne o over65");
+const namePassengersEl = document.getElementById("name");
+const totalKmEl = document.getElementById("km");
+const passengersAgeRangeEl = document.getElementById("age");
+const buttonDOMEl = document.getElementById("test");
 
-const namePassengers = document.getElementById("name");
-const kmPassengers = document.getElementById("km");
-const passengersAgeRange = document.getElementById("age");
-console.log(passengersAgeRange);
-
-namePassengers.value = askName;
-kmPassengers.value = totalKm;
-passengersAgeRange.value = passengerAge;
-
-console.log(namePassengers.value);
-console.log(kmPassengers.value);
-console.log(passengersAgeRange.value);
-
-console.log(namePassengers);
-console.log(kmPassengers);
-console.log(passengersGen);
-
-// console.log("kmPrice", kmPrice);
-// console.log("totalKm", totalKm);
-// console.log("passengerAge", passengerAge);
+// console.log(kmPrice);
+// console.log(buttonDOMEl);
+// console.log(passengersAgeRange);
 
 // ^ SVOLGIMENTO
 
-// let price = totalKm * kmPrice;
+buttonDOMEl.addEventListener("click", () => {
+  const totalKm = parseInt(totalKmEl.value); // int
+  let passengerAge = passengersAgeRangeEl.value;
+  let price = totalKm * kmPrice;
 
-// console.log(price);
+  if (passengerAge === "minorenne") {
+    console.log("l'utente è minorenne");
 
-// if (passengerAge < 18) {
-//   console.log("l'utente è minorenne");
+    price = price * 0.8;
+  } else if (passengerAge === "over65") {
+    console.log("l'utente è over65");
 
-//   price = price * 0.8;
-// } else if (passengerAge > 65) {
-//   console.log("l'utente è over");
+    price = price * 0.6;
+  }
 
-//   price = price * 0.6;
-// }
-
-// console.log(`hai acquistato al prezzo ${price.toFixed(2)}€`);
+  console.log(namePassengersEl.value);
+  console.log(totalKmEl.value);
+  console.log(passengersAgeRangeEl.value);
+  console.log(`hai acquistato al prezzo ${price.toFixed(2)}€`);
+});
