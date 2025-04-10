d36d8722abaf4879778aac6fff3f63744fe11649
maurizio-dominici <mauriziodominici4@gmail.com>
2025-04-10 18:02:14 +0200

    add card hidden

--- biglietto-treno/js/main.js
+++ biglietto-treno/js/main.js
@@ -3,15 +3,19 @@ const kmPrice = 0.21;
 const namePassengersEl = document.getElementById("name");
 const totalKmEl = document.getElementById("km");
 const passengersAgeRangeEl = document.getElementById("age");
-const buttonDOMEl = document.getElementById("test");
+const buttonEl = document.getElementById("test");
+const formEl = document.getElementById("form-train");
+const cardHiddenEl = document.getElementById("card-hidden");
 
 // console.log(kmPrice);
 // console.log(buttonDOMEl);
 // console.log(passengersAgeRange);
+// console.log(formEl);
 
 // ^ SVOLGIMENTO
 
-buttonDOMEl.addEventListener("click", () => {
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
   const totalKm = parseInt(totalKmEl.value); // int
   let passengerAge = passengersAgeRangeEl.value;
   let price = totalKm * kmPrice;
@@ -26,8 +30,22 @@ buttonDOMEl.addEventListener("click", () => {
     price = price * 0.6;
   }
 
-  console.log(namePassengersEl.value);
-  console.log(totalKmEl.value);
-  console.log(passengersAgeRangeEl.value);
-  console.log(`hai acquistato al prezzo ${price.toFixed(2)}€`);
+  // console.log(namePassengersEl.value);
+  // console.log(totalKmEl.value);
+  // console.log(passengersAgeRangeEl.value);
+  // console.log(`hai acquistato al prezzo ${price.toFixed(2)}€`);
+  cardHiddenEl.innerHTML = `
+    <div class="col">
+      <div class="card">
+        <div class="card-header">
+          ${namePassengersEl.value}
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">${`hai acquistato al prezzo ${price.toFixed(
+            2
+          )}€`}</h5>
+        </div>
+      </div>
+    </div>
+  `;
 });
